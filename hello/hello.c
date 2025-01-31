f7ce887bb880a529d463e73126da176c12978127
akotu <akotu@protonmail.com>
2025-01-31 23:13:13 +0100

    Added loop to hello.c to extend performance

--- hello/hello.c
+++ hello/hello.c
@@ -1,21 +1,23 @@
 #include "mpi.h"  /* wlacza plik naglowkowy potrzebny dla biblioteki MPI */
 #include <stdio.h> /* potrzebny dla funkcji 'fprintf()' */
+#include <unistd.h> /* potrzebny dla funkcji 'sleep()' */
 
-int main(int argc, char **argv)
-{
-	int myid, namelen; 
+int main(int argc, char **argv) {
+	int myid, namelen;
 	char computer_name[512]; /* nazwa komputera na ktorym wykonuje sie proces */
 
 	MPI_Init(&argc, &argv);  /* inicjalizuje srodowisko MPI */
 
 	/* pobiera identyfikator biezacego procesu */
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid); 
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
 	/* Pobiera nazwe komputera na ktorym proces sie wykonuje */
  	MPI_Get_processor_name(computer_name, &namelen);
 
- 	printf("Hello from process %d on %s\n", myid, computer_name);
-
+	for (int i = 0; i < 100; i++) {
+		printf("Hello from process %d on %s\n", myid, computer_name);
+		sleep(1);
+	}
 	/* konczy dzialanie srodowiska MPI */
 	MPI_Finalize();
 
